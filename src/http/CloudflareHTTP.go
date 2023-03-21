62cd79d9556f94ed6aa56fa7eccfa7cc84df34a4
root <root@amazing-agnesi.77-68-32-53.plesk.page>
2023-03-21 17:24:03 +0000

    Added comments to code

--- src/http/CloudflareHTTP.go
+++ src/http/CloudflareHTTP.go
@@ -13,24 +13,29 @@ const UrlBase string = "https://api.cloudflare.com/client/v4/graphql"
 
 //getReq() is a private function which will perform a simple get request
 func getReq(urlPath, authToken string) (string, error) {
+	//Define the HTTP method to use
 	method := "GET"
 
+	//Define the full URL
 	url := UrlBase + urlPath
 
+	//Create the HTTP client and request, and check and return errors
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, nil)
-
 	if err != nil {
 		return "", err
 	}
+	//Add the API header as an authorization header
 	req.Header.Add("Authorization", authToken)
 
+	//Execute the request and check for errors
 	res, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer res.Body.Close()
 
+	//Return as string anc check for errors
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return "", err
@@ -40,25 +45,31 @@ func getReq(urlPath, authToken string) (string, error) {
 
 //postReq is a private function which performs a simple post request
 func postReq(urlPath, authToken, payload string) (string, error) {
+	//Define the HTTP method to use
 	method := "POST"
 
+	//Define the full URL
 	url := UrlBase + urlPath
+
+	//Create the HTTP client and request, and check for errors
 	client := &http.Client{}
 	//Convert the string payload into a ioreader
 	reqBody := strings.NewReader(payload)
-
 	req, err := http.NewRequest(method, url, reqBody)
 	if err != nil {
 		return "", err
 	}
+	//Add the API header as an authorization header
 	req.Header.Add("Authorization", authToken)
 
+	//Execute the request and check for errors
 	res, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer res.Body.Close()
 
+	//Return as string anc check for errors
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return "", err
